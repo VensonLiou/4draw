93e1283819c29e44022ce33fe5a5bd98b6911e8a
Pi <pi@teahouse.finance>
2024-06-17 01:25:58 +0800

    :sparkles: create StepTitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import StepTitle from "@/components/Text/StepTitle";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      home page
+      <StepTitle>1. Choose your numbers</StepTitle>
     </main>
   );
 }

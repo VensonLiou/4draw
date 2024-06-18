1ef5f8c8c0c79495d498bd69c6f68e739ba60d90
Pi <pi@teahouse.finance>
2024-06-18 22:01:42 +0800

    :pencil: fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import NumberSelectGroup from "@/components/NumberSelectGroup/NumberSelectGroup"
 import StepTitle from "@/components/Text/StepTitle";
 import { HStack } from "@chakra-ui/react";
 import styles from "./page.module.css";
-import ContentContaner from "@/components/ContentContaner";
+import ContentContainer from "@/components/ContentContainer";
 import StepContainer from "@/components/StepContainer";
 import StepContentContainer from "@/components/StepContentContainer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <ContentContaner>
+      <ContentContainer>
         <StepContainer>
           <StepTitle step={1} title="Choose your numbers" />
           <StepContentContainer>
@@ -21,7 +21,7 @@ export default function Home() {
             </HStack>
           </StepContentContainer>
         </StepContainer>
-      </ContentContaner>
+      </ContentContainer>
     </main>
   );
 }

e8c20abcec68fdff10ef5ec8947ccc9c314b1248
Pi <pi@teahouse.finance>
2024-06-20 23:03:42 +0800

    :zap: update LastRoundPage

--- src/hooks/useGameInfo.ts
+++ src/hooks/useGameInfo.ts
@@ -1,4 +1,5 @@
 import ABI from "@/configs/ABI"
+import { MOCK_GAME_INFO } from "@/mocks/mocks"
 import { bigintishToArray } from "@/utils/utils"
 import { useAccount, useContractRead } from "@starknet-react/core"
 import { BlockTag } from "starknet"
@@ -18,6 +19,7 @@ const useGameInfo = () => {
   const { data: data2 } = read({ ...opt, functionName: 'game_info', args: [String(data1)!], enabled: B(data1) })
   const { data: data3 } = read({ ...opt, functionName: 'user_latest_round', args: [userAddress!], enabled: B(userAddress) })
   const { data: data4 } = read({ ...opt, functionName: 'latest_tickets_result', args: [userAddress!], enabled: B(userAddress) })
+  const { data: data5 } = read({ ...opt, functionName: 'game_info', args: [String(data3)!], enabled: B(data3) })
 
   // console.log('/ /// / /// / ///')
   // console.log(['latest_game_round', data1])
@@ -39,6 +41,8 @@ const useGameInfo = () => {
     data4 ? data4 : {}
   ) as { 0?: bigint, 1?: boolean, 2?: UserTicketInfo, 3?: bigint }
 
+  const userLatestRoundResult = ((Boolean(userLatestRound) ? data5 : {}) as { result_number?: bigint }).result_number
+
   const userTickets = _userTickets && {
     picked_number: bigintishToArray(_userTickets.picked_number),
     claimed: _userTickets.claimed,
@@ -48,28 +52,39 @@ const useGameInfo = () => {
     mini_amount: Number(_userTickets.mini_amount),
   }
 
-  return {
-    latestGameRound: data1 === undefined ? undefined : Number(data1),
-    gameInfo: data2 ? {
-      end_time: Number(end_time),
-      game_status: game_status.activeVariant() as GameStatus,
-      result_number: bigintishToArray(result_number),
-      ticket_price: ticket_price as bigint,
-      total_box_prize_accumulated: total_box_prize_accumulated as bigint,
-      total_mini_prize_accumulated: total_mini_prize_accumulated as bigint,
-      total_straight_prize_accumulated: total_straight_prize_accumulated as bigint
-    } : undefined,
-    userLatestRound: data3 === undefined ? undefined : Number(data3),
-    latestTicketsResult: {
-      userLatestRound: Number(userLatestRound),
-      revealed,
-      userTickets,
-      unclaimed_prize
-    }
-  }
+  return MOCK_GAME_INFO
+  // return {
+  //   latestGameRound: data1 === undefined ? undefined : Number(data1),
+  //   gameInfo: data2 ? {
+  //     end_time: Number(end_time),
+  //     game_status: game_status.activeVariant() as GameStatus,
+  //     latest_result_number: bigintishToArray(result_number),
+  //     ticket_price: ticket_price as bigint,
+  //     total_box_prize_accumulated: total_box_prize_accumulated as bigint,
+  //     total_mini_prize_accumulated: total_mini_prize_accumulated as bigint,
+  //     total_straight_prize_accumulated: total_straight_prize_accumulated as bigint
+  //   } : undefined,
+  //   latestTicketsResult: {
+  //     userLatestRound: Number(userLatestRound),
+  //     revealed,
+  //     userTickets,
+  //     unclaimed_prize,
+  //     userLatestRoundResult: userLatestRoundResult === undefined ? undefined : bigintishToArray(userLatestRoundResult)
+  //   }
+  // }
 }
 
 export default useGameInfo
+
+export type GameInfo = {
+  end_time: number;
+  game_status: GameStatus;
+  result_number: number[] | undefined;
+  ticket_price: bigint;
+  total_box_prize_accumulated: bigint;
+  total_mini_prize_accumulated: bigint;
+  total_straight_prize_accumulated: bigint;
+}
 
 type GameStatus = 'NotStarted' | 'Started' | 'Revealing' | 'Ended'
 

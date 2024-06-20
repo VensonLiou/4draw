ea3dff501777ed0ad3fbbba2995b1df01ca653f4
Pi <pi@teahouse.finance>
2024-06-20 08:13:28 +0800

    :zap: handle page flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,24 @@ import styles from "./page.module.css";
 export default function Home() {
   const [pageName] = usePage()
 
+  // TODO: 檢查有沒有前一輪
+  const isFirstRound = true
+
+  // TODO: 判斷是否已經開獎
+  const isPrizeRevealed = true
+
+  let redirect: PageName = pageName
+
+  // 沒有前一輪，直接去選號頁
+  if (pageName === 'last-round' && isFirstRound) redirect = 'choose-number'
+  
+  // 有前一輪，但未開獎，改去開獎頁面
+  else if (!isPrizeRevealed) redirect = 'open-prize'
+
+
   return (
     <main className={styles.main}>
-      {PAGE_MAP[pageName]}
+      {PAGE_MAP[redirect]}
     </main>
   );
 }

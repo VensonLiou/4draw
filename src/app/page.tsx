159fdbd78c793789257e80a4608f912b23a831fc
Pi <pi@teahouse.finance>
2024-06-20 12:21:53 +0800

    :zap: update LastRoundPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,18 +12,32 @@ export default function Home() {
   const [pageName] = usePage()
 
   // TODO: 檢查有沒有前一輪
-  const isFirstRound = true
+  const isFirstRound = false
 
   // TODO: 判斷是否已經開獎
-  const isPrizeRevealed = true
+  const winningNumber = [1, 2, 3, 4]
+  // const winningNumber = undefined
+  const isPrizeRevealed = Boolean(winningNumber)
+
 
   let redirect: PageName = pageName
 
   // 沒有前一輪，直接去選號頁
   if (pageName === 'last-round' && isFirstRound) redirect = 'choose-number'
-  
+
   // 有前一輪，但未開獎，改去開獎頁面
   else if (!isPrizeRevealed) redirect = 'open-prize'
+
+
+  const PAGE_MAP: { [page in PageName]: ReactNode } = {
+    "last-round": <LastRoundPage />,
+    "choose-number": <ChooseNumberPage />,
+    "choose-bet-type": <ChooseBetTypePage />,
+    "place-bet": <PlaceBetPage />,
+    "bet-placed": <BetPlacedPage />,
+    "open-prize": "open-prize",
+    "claim": "claim",
+  }
 
 
   return (
@@ -31,14 +45,4 @@ export default function Home() {
       {PAGE_MAP[redirect]}
     </main>
   );
-}
-
-const PAGE_MAP: { [page in PageName]: ReactNode } = {
-  "last-round": <LastRoundPage />,
-  "choose-number": <ChooseNumberPage />,
-  "choose-bet-type": <ChooseBetTypePage />,
-  "place-bet": <PlaceBetPage />,
-  "bet-placed": <BetPlacedPage />,
-  "open-prize": "open-prize",
-  "claim": "claim",
 }

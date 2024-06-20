351370ff5de324f6be19d1d8f97737802cb3d8fc
Pi <pi@teahouse.finance>
2024-06-21 01:37:50 +0800

    :art: make toast dark

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,16 +1,21 @@
 'use client'
+import { COLOR } from '@/css/Colors'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ReactNode } from 'react'
+import { Toaster } from 'react-hot-toast'
 import StarknetProvider from './StarknetProvider'
 import { themes } from './themes'
-import { Toaster, resolveValue, } from 'react-hot-toast'
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <StarknetProvider>
       <ChakraProvider resetCSS={false} theme={themes}>
         {children}
-        <Toaster />
+        <Toaster
+          toastOptions={{
+            style: { backgroundColor: '#333', color: COLOR.White }
+          }}
+        />
       </ChakraProvider>
     </StarknetProvider>
   )

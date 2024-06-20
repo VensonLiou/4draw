16db4638680548c2ee6d5232137bfff7b81b89d5
Pi <pi@teahouse.finance>
2024-06-20 23:39:11 +0800

    :zap: move <Toast /> info <Providers />

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -3,12 +3,14 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 import StarknetProvider from './StarknetProvider'
 import { themes } from './themes'
+import { Toaster, resolveValue, } from 'react-hot-toast'
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <StarknetProvider>
       <ChakraProvider resetCSS={false} theme={themes}>
         {children}
+        <Toaster />
       </ChakraProvider>
     </StarknetProvider>
   )

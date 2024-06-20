320580258e0072cd7bde2a7abf40ae47ffc37f6b
Pi <pi@teahouse.finance>
2024-06-20 16:34:29 +0800

    :zap: update useGameInfo

--- src/hooks/useGameInfo.ts
+++ src/hooks/useGameInfo.ts
@@ -1,41 +1,58 @@
 import ABI from "@/configs/ABI"
 import { useAccount, useContractRead } from "@starknet-react/core"
-import { BlockTag } from "starknet"
+import { BlockTag, CairoCustomEnum, num } from "starknet"
 
 const useGameInfo = (round?: number, pickedNumber?: number) => {
   const { address: userAddress } = useAccount()
 
   const opt = {
     blockIdentifier: BlockTag.PENDING, // important
-    abi: ABI as any,
-    address: '0x'
+    abi: ABI,
+    address: process.env.NEXT_CONTRACT_ADDRESS
   }
 
   const read = useContractRead
   const B = Boolean
-  const { data: data1 } = read({ ...opt, functionName: 'randomness_contract' })
-  const { data: data2 } = read({ ...opt, functionName: 'ticket_payment_token' })
-  const { data: data3 } = read({ ...opt, functionName: 'reveal_config' })
-  const { data: data4 } = read({ ...opt, functionName: 'latest_game_round' })
-  const { data: data5 } = read({ ...opt, functionName: 'game_info', args: [round!], enabled: B(round) })
-  const { data: data6 } = read({ ...opt, functionName: 'ticket_counter', args: [round!, pickedNumber!], enabled: B(round) && B(pickedNumber) })
-  const { data: data7 } = read({ ...opt, functionName: 'user_latest_round', args: [userAddress!], enabled: B(userAddress) })
-  const { data: data8 } = read({ ...opt, functionName: 'user_tickets', args: [userAddress!], enabled: B(userAddress) })
-  const { data: data9 } = read({ ...opt, functionName: 'latest_tickets_result', args: [userAddress!], enabled: B(userAddress) })
+  const { data: data1 } = read({ ...opt, functionName: 'latest_game_round' })
+  const { data: data2 } = read({ ...opt, functionName: 'game_info', args: [String(data1)!], enabled: B(data1) })
+  const { data: data3 } = read({ ...opt, functionName: 'user_latest_round', args: [userAddress!], enabled: B(userAddress) })
+  const { data: data4 } = read({ ...opt, functionName: 'user_tickets', args: [userAddress!], enabled: B(userAddress) })
+  const { data: data5 } = read({ ...opt, functionName: 'latest_tickets_result', args: [userAddress!], enabled: B(userAddress) })
 
+  // console.log('/ /// / /// / ///')
+  // console.log(['latest_game_round', data1])
+  // console.log(['game_info', data2])
+  // console.log(['user_latest_round', data3])
+  // console.log(['user_tickets', data4])
+  // console.log(['latest_tickets_result', data5])
 
+  const {
+    end_time,
+    game_status,
+    result_number,
+    ticket_price,
+    total_box_prize_accumulated,
+    total_mini_prize_accumulated,
+    total_straight_prize_accumulated
+  } = data2 as any ?? {}
 
   return {
-    randomnessContract: data1,
-    paymentTokenAddress: data2,
-    revealConfig: data3,
-    latestGameRound: data4,
-    gameInfo: data5,
-    ticketCounter: data6,
-    userLatestRound: data7,
-    userTickets: data8,
-    latestTicketsResult: data9
+    latestGameRound: data1 as undefined | bigint,
+    gameInfo: data2 ? {
+      end_time: Number(end_time),
+      game_status: game_status.activeVariant() as GameStatus,
+      result_number: Array.from(String(result_number), Number),
+      ticket_price: ticket_price as bigint,
+      total_box_prize_accumulated: total_box_prize_accumulated as bigint,
+      total_mini_prize_accumulated: total_mini_prize_accumulated as bigint,
+      total_straight_prize_accumulated: total_straight_prize_accumulated as bigint
+    } : undefined,
+    userLatestRound: data3 as undefined | bigint,
+    userTickets: data4 as undefined | bigint,
+    latestTicketsResult: data5 as undefined | bigint
   }
 }
 
 export default useGameInfo
+
+type GameStatus = 'NotStarted' | 'Started' | 'Revealing' | 'Ended' 

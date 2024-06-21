0bddd384cec503b3848e69a397f36f73b7e53629
Pi <pi@teahouse.finance>
2024-06-21 21:37:11 +0800

    :zap: BetPlacedPage

--- src/subpages/BetPlacedPage.tsx
+++ src/subpages/BetPlacedPage.tsx
@@ -1,11 +1,16 @@
+import useGameInfo from '@/hooks/useGameInfo'
+import { formatDate } from '@/utils/utils'
 import { Stack } from '@chakra-ui/react'
 
 const BetPlacedPage = () => {
+  const { latestGameRound, gameInfo } = useGameInfo()
+
+  const endTimeString = formatDate(gameInfo.end_time, 'HH:MM mmmm dd, yyyy')
 
   return (
     <Stack alignItems={'center'} gap={0} >
       <h1 style={{ marginBlock: '80px 40px', fontSize: 48, lineHeight: '56px', fontWeight: 700 }}>Bet Placed!</h1>
-      <p style={{ fontSize: 20, lineHeight: '28px', fontWeight: 700 }}>Winning numbers for Round #123 will be revealed at 15:00 June 23, 2024.</p>
+      <p style={{ fontSize: 20, lineHeight: '28px', fontWeight: 700 }}>Winning numbers for Round #{latestGameRound ?? ''} will be revealed at {endTimeString}.</p>
     </Stack>
   )
 }

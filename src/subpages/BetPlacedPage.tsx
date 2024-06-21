99220049a6e32784a355d6cc1df4107c964f2280
Pi <pi@teahouse.finance>
2024-06-21 21:53:11 +0800

    :pencil: update wording

--- src/subpages/BetPlacedPage.tsx
+++ src/subpages/BetPlacedPage.tsx
@@ -10,7 +10,7 @@ const BetPlacedPage = () => {
   return (
     <Stack alignItems={'center'} gap={0} >
       <h1 style={{ marginBlock: '80px 40px', fontSize: 48, lineHeight: '56px', fontWeight: 700 }}>Bet Placed!</h1>
-      <p style={{ fontSize: 20, lineHeight: '28px', fontWeight: 700 }}>Winning numbers for Round #{latestGameRound ?? ''} will be revealed at {endTimeString}.</p>
+      <p style={{ fontSize: 20, lineHeight: '28px', fontWeight: 700 }}>Winning numbers for Round #{latestGameRound ?? ''} can be revealed at {endTimeString}.</p>
     </Stack>
   )
 }
